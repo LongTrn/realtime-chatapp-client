bc6fb119063a5d14823265d130931181a1d15b87
Long <longjax1401@gmail.com>
2021-05-10 14:41:23 +0700

    change dotenv files and axios config

--- src/services/base.server.js
+++ src/services/base.server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default axios.create({
+export default axios.default({
     baseURL: process.env.REACT_APP_SERVER_URL,
     headers: {
         "Content-type": "application/json",
